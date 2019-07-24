c1cb1fe7002fc53704ed4c0b38e194c8f2d0cc35
Nicholas Serra <nickserra@gmail.com>
2019-07-24 15:07:07 -0400

    Stats counts plaintext

---
+++ ngx_rtmp_stat_counts_module.c
@@ -0,0 +1,398 @@
+
+/*
+ * Copyright (C) Roman Arutyunyan
+ */
+
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <ngx_http.h>
+#include <nginx.h>
+#include "ngx_rtmp.h"
+#include "ngx_rtmp_version.h"
+#include "ngx_rtmp_live_module.h"
+#include "ngx_rtmp_play_module.h"
+#include "ngx_rtmp_codec_module.h"
+
+
+static ngx_int_t ngx_rtmp_stat_init_process(ngx_cycle_t *cycle);
+static char *ngx_rtmp_stat(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
+static ngx_int_t ngx_rtmp_stat_postconfiguration(ngx_conf_t *cf);
+static void * ngx_rtmp_stat_create_loc_conf(ngx_conf_t *cf);
+static char * ngx_rtmp_stat_merge_loc_conf(ngx_conf_t *cf,
+        void *parent, void *child);
+
+
+static time_t                       start_time;
+
+
+#define NGX_RTMP_STAT_ALL           0xff
+#define NGX_RTMP_STAT_GLOBAL        0x01
+#define NGX_RTMP_STAT_LIVE          0x02
+#define NGX_RTMP_STAT_CLIENTS       0x04
+#define NGX_RTMP_STAT_PLAY          0x08
+
+/*
+ * global: stat-{bufs-{total,free,used}, total bytes in/out, bw in/out} - cscf
+*/
+
+
+typedef struct {
+    ngx_uint_t                      stat;
+    ngx_str_t                       stylesheet;
+} ngx_rtmp_stat_loc_conf_t;
+
+
+static ngx_conf_bitmask_t           ngx_rtmp_stat_masks[] = {
+    { ngx_string("all"),            NGX_RTMP_STAT_ALL           },
+    { ngx_string("global"),         NGX_RTMP_STAT_GLOBAL        },
+    { ngx_string("live"),           NGX_RTMP_STAT_LIVE          },
+    { ngx_string("clients"),        NGX_RTMP_STAT_CLIENTS       },
+    { ngx_null_string,              0 }
+};
+
+
+static ngx_command_t  ngx_rtmp_stat_commands[] = {
+
+    { ngx_string("rtmp_stat_counts"),
+        NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_1MORE,
+        ngx_rtmp_stat,
+        NGX_HTTP_LOC_CONF_OFFSET,
+        offsetof(ngx_rtmp_stat_loc_conf_t, stat),
+        ngx_rtmp_stat_masks },
+
+    ngx_null_command
+};
+
+
+static ngx_http_module_t  ngx_rtmp_stat_module_ctx = {
+    NULL,                               /* preconfiguration */
+    ngx_rtmp_stat_postconfiguration,    /* postconfiguration */
+
+    NULL,                               /* create main configuration */
+    NULL,                               /* init main configuration */
+
+    NULL,                               /* create server configuration */
+    NULL,                               /* merge server configuration */
+
+    ngx_rtmp_stat_create_loc_conf,      /* create location configuration */
+    ngx_rtmp_stat_merge_loc_conf,       /* merge location configuration */
+};
+
+
+ngx_module_t  ngx_rtmp_stat_counts_module = {
+    NGX_MODULE_V1,
+    &ngx_rtmp_stat_module_ctx,          /* module context */
+    ngx_rtmp_stat_commands,             /* module directives */
+    NGX_HTTP_MODULE,                    /* module type */
+    NULL,                               /* init master */
+    NULL,                               /* init module */
+    ngx_rtmp_stat_init_process,         /* init process */
+    NULL,                               /* init thread */
+    NULL,                               /* exit thread */
+    NULL,                               /* exit process */
+    NULL,                               /* exit master */
+    NGX_MODULE_V1_PADDING
+};
+
+
+#define NGX_RTMP_STAT_BUFSIZE           256
+
+
+static ngx_int_t
+ngx_rtmp_stat_init_process(ngx_cycle_t *cycle)
+{
+    /*
+     * HTTP process initializer is called
+     * after event module initializer
+     * so we can run posted events here
+     */
+
+    ngx_event_process_posted(cycle, &ngx_rtmp_init_queue);
+
+    return NGX_OK;
+}
+
+
+/* ngx_escape_html does not escape characters out of ASCII range
+ * which are bad for xslt */
+
+static void *
+ngx_rtmp_stat_escape(ngx_http_request_t *r, void *data, size_t len)
+{
+    u_char *p, *np;
+    void   *new_data;
+    size_t  n;
+
+    p = data;
+
+    for (n = 0; n < len; ++n, ++p) {
+        if (*p < 0x20 || *p >= 0x7f) {
+            break;
+        }
+    }
+
+    if (n == len) {
+        return data;
+    }
+
+    new_data = ngx_palloc(r->pool, len);
+    if (new_data == NULL) {
+        return NULL;
+    }
+
+    p  = data;
+    np = new_data;
+
+    for (n = 0; n < len; ++n, ++p, ++np) {
+        *np = (*p < 0x20 || *p >= 0x7f) ? (u_char) ' ' : *p;
+    }
+
+    return new_data;
+}
+
+#if (NGX_WIN32)
+/*
+ * Fix broken MSVC memcpy optimization for 4-byte data
+ * when this function is inlined
+ */
+__declspec(noinline)
+#endif
+
+static void
+ngx_rtmp_stat_output(ngx_http_request_t *r, ngx_chain_t ***lll,
+        void *data, size_t len, ngx_uint_t escape)
+{
+    ngx_chain_t        *cl;
+    ngx_buf_t          *b;
+    size_t              real_len;
+
+    if (len == 0) {
+        return;
+    }
+
+    if (escape) {
+        data = ngx_rtmp_stat_escape(r, data, len);
+        if (data == NULL) {
+            return;
+        }
+    }
+
+    real_len = escape
+        ? len + ngx_escape_html(NULL, data, len)
+        : len;
+
+    cl = **lll;
+    if (cl && cl->buf->last + real_len > cl->buf->end) {
+        *lll = &cl->next;
+    }
+
+    if (**lll == NULL) {
+        cl = ngx_alloc_chain_link(r->pool);
+        if (cl == NULL) {
+            return;
+        }
+        b = ngx_create_temp_buf(r->pool,
+                ngx_max(NGX_RTMP_STAT_BUFSIZE, real_len));
+        if (b == NULL || b->pos == NULL) {
+            return;
+        }
+        cl->next = NULL;
+        cl->buf = b;
+        **lll = cl;
+    }
+
+    b = (**lll)->buf;
+
+    if (escape) {
+        b->last = (u_char *)ngx_escape_html(b->last, data, len);
+    } else {
+        b->last = ngx_cpymem(b->last, data, len);
+    }
+}
+
+
+/* These shortcuts assume 2 variables exist in current context:
+ *   ngx_http_request_t    *r
+ *   ngx_chain_t         ***lll */
+
+/* plain data */
+#define NGX_RTMP_STAT(data, len)    ngx_rtmp_stat_output(r, lll, data, len, 0)
+
+/* escaped data */
+#define NGX_RTMP_STAT_E(data, len)  ngx_rtmp_stat_output(r, lll, data, len, 1)
+
+/* literal */
+#define NGX_RTMP_STAT_L(s)          NGX_RTMP_STAT((s), sizeof(s) - 1)
+
+/* ngx_str_t */
+#define NGX_RTMP_STAT_S(s)          NGX_RTMP_STAT((s)->data, (s)->len)
+
+/* escaped ngx_str_t */
+#define NGX_RTMP_STAT_ES(s)         NGX_RTMP_STAT_E((s)->data, (s)->len)
+
+/* C string */
+#define NGX_RTMP_STAT_CS(s)         NGX_RTMP_STAT((s), ngx_strlen(s))
+
+/* escaped C string */
+#define NGX_RTMP_STAT_ECS(s)        NGX_RTMP_STAT_E((s), ngx_strlen(s))
+
+
+#define NGX_RTMP_STAT_BW            0x01
+#define NGX_RTMP_STAT_BYTES         0x02
+#define NGX_RTMP_STAT_BW_BYTES      0x03
+
+
+static void
+ngx_rtmp_stat_server(ngx_http_request_t *r, ngx_chain_t ***lll,
+        ngx_rtmp_core_srv_conf_t *cscf)
+{
+    ngx_rtmp_core_app_conf_t      **cacf;
+    size_t                          n;
+
+    cacf = cscf->applications.elts;
+    for (n = 0; n < cscf->applications.nelts; ++n, ++cacf) {
+
+        ngx_rtmp_stat_loc_conf_t       *slcf;
+
+        ngx_rtmp_core_app_conf_t *cacf2;
+        cacf2 = *cacf;
+
+        NGX_RTMP_STAT_ES(&cacf2->name);
+        NGX_RTMP_STAT_L(" ");
+
+        slcf = ngx_http_get_module_loc_conf(r, ngx_rtmp_stat_counts_module);
+
+        if (slcf->stat & NGX_RTMP_STAT_LIVE) {
+
+            ngx_rtmp_live_app_conf_t *lacf;
+
+            lacf = cacf2->app_conf[ngx_rtmp_live_module.ctx_index];
+
+            ngx_rtmp_live_stream_t         *stream;
+            ngx_rtmp_live_ctx_t            *ctx;
+            ngx_int_t                       n;
+            ngx_uint_t                      nclients, total_nclients;
+            u_char                          buf[NGX_INT_T_LEN];
+
+            if (!lacf->live) {
+                return;
+            }
+
+
+            total_nclients = 0;
+            for (n = 0; n < lacf->nbuckets; ++n) {
+                for (stream = lacf->streams[n]; stream; stream = stream->next) {
+
+                    nclients = 0;
+                    for (ctx = stream->ctx; ctx; ctx = ctx->next, ++nclients) {
+
+                    }
+                    total_nclients += nclients;
+                }
+            }
+
+            NGX_RTMP_STAT(buf, ngx_snprintf(buf, sizeof(buf),
+                          "%ui", total_nclients) - buf);
+            NGX_RTMP_STAT_L(" ");
+
+        }
+    }
+}
+
+
+static ngx_int_t
+ngx_rtmp_stat_handler(ngx_http_request_t *r)
+{
+    ngx_rtmp_stat_loc_conf_t       *slcf;
+    ngx_rtmp_core_main_conf_t      *cmcf;
+    ngx_rtmp_core_srv_conf_t      **cscf;
+    ngx_chain_t                    *cl, *l, **ll, ***lll;
+    size_t                          n;
+    off_t                           len;
+
+    slcf = ngx_http_get_module_loc_conf(r, ngx_rtmp_stat_counts_module);
+    if (slcf->stat == 0) {
+        return NGX_DECLINED;
+    }
+
+    cmcf = ngx_rtmp_core_main_conf;
+    if (cmcf == NULL) {
+        goto error;
+    }
+
+    cl = NULL;
+    ll = &cl;
+    lll = &ll;
+
+    cscf = cmcf->servers.elts;
+    for (n = 0; n < cmcf->servers.nelts; ++n, ++cscf) {
+        ngx_rtmp_stat_server(r, lll, *cscf);
+    }
+
+    len = 0;
+    for (l = cl; l; l = l->next) {
+        len += (l->buf->last - l->buf->pos);
+    }
+    ngx_str_set(&r->headers_out.content_type, "text/plain");
+    r->headers_out.content_length_n = len;
+    r->headers_out.status = NGX_HTTP_OK;
+    ngx_http_send_header(r);
+    (*ll)->buf->last_buf = 1;
+    return ngx_http_output_filter(r, cl);
+
+error:
+    r->headers_out.status = NGX_HTTP_INTERNAL_SERVER_ERROR;
+    r->headers_out.content_length_n = 0;
+    return ngx_http_send_header(r);
+}
+
+
+static void *
+ngx_rtmp_stat_create_loc_conf(ngx_conf_t *cf)
+{
+    ngx_rtmp_stat_loc_conf_t       *conf;
+
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_rtmp_stat_loc_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+
+    conf->stat = 0;
+
+    return conf;
+}
+
+
+static char *
+ngx_rtmp_stat_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
+{
+    ngx_rtmp_stat_loc_conf_t       *prev = parent;
+    ngx_rtmp_stat_loc_conf_t       *conf = child;
+
+    ngx_conf_merge_bitmask_value(conf->stat, prev->stat, 0);
+    ngx_conf_merge_str_value(conf->stylesheet, prev->stylesheet, "");
+
+    return NGX_CONF_OK;
+}
+
+
+static char *
+ngx_rtmp_stat(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+{
+    ngx_http_core_loc_conf_t  *clcf;
+
+    clcf = ngx_http_conf_get_module_loc_conf(cf, ngx_http_core_module);
+    clcf->handler = ngx_rtmp_stat_handler;
+
+    return ngx_conf_set_bitmask_slot(cf, cmd, conf);
+}
+
+
+static ngx_int_t
+ngx_rtmp_stat_postconfiguration(ngx_conf_t *cf)
+{
+    start_time = ngx_cached_time->sec;
+
+    return NGX_OK;
+}
